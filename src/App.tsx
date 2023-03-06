47a45a32fe673777321e103bb949a1966424f0f4
Zach Robin <zrobin144@gmail.com>
2023-03-06 11:53:08 -0800

    Initial Commit
    Initialized React

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
+import { Container } from '@mui/system';
+
+
+
+function App() {
+  return (
+    <Box >
+        <div className='App'>
+          <Container className='NavContainer' maxWidth='xl'>
+            <div className='containerg'>
+              <div className='logo'>
+                <h1>REMS SMART on FHIR App</h1>
+              </div>
+            </div>
+          </Container>
+        </div>
+    </Box >
+  );
+}
+
+export default App;
