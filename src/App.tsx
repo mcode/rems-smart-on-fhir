0b080aaebe90bc1624aa014620cc447636c38f34
Zach Robin <zrobin144@gmail.com>
2023-03-06 12:19:43 -0800

    react app initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@mui/material';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';import { Button } from '@mui/material';
 import Box from '@mui/material/Box';
 import { Container } from '@mui/system';
+import './App.css';
 
 
 
@@ -11,6 +12,7 @@ function App() {
           <Container className='NavContainer' maxWidth='xl'>
             <div className='containerg'>
               <div className='logo'>
+                <ContentPasteIcon sx={{ color: 'white', fontSize: 40, paddingTop: 2.5, paddingRight: 2.5 }} />
                 <h1>REMS SMART on FHIR App</h1>
               </div>
             </div>
