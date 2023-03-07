aa31e3502d3dcd686fa55a83b481f8a0188991ad
Zach Robin <zrobin144@gmail.com>
2023-03-06 16:37:12 -0800

    Smart on FHIR app w/ patient context launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import ContentPasteIcon from '@mui/icons-material/ContentPaste';import { Button } from '@mui/material';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
 import Box from '@mui/material/Box';
 import { Container } from '@mui/system';
 import './App.css';
+import Patient from './views/Patient/Patient';
 
+function App(props: any) {
+  const client = props.client;
 
-
-function App() {
   return (
-    <Box >
-        <div className='App'>
-          <Container className='NavContainer' maxWidth='xl'>
-            <div className='containerg'>
-              <div className='logo'>
-                <ContentPasteIcon sx={{ color: 'white', fontSize: 40, paddingTop: 2.5, paddingRight: 2.5 }} />
-                <h1>REMS SMART on FHIR App</h1>
-              </div>
+    <Box>
+      <div className='App'>
+        <Container className='NavContainer' maxWidth='xl'>
+          <div className='containerg'>
+            <div className='logo'>
+              <ContentPasteIcon sx={{ color: 'white', fontSize: 50, paddingTop: .5, paddingRight: 2.5 }} />
+              <h1>REMS SMART on FHIR App</h1>
             </div>
-          </Container>
-        </div>
+          </div>
+        </Container>
+      </div>
+      <Patient client={client}/>
     </Box >
   );
 }
