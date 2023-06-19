6c3a623c7a3210b3a866357b8cd99ab4c6893b41
Patrick LaRocque <plarocque@mitre.org>
2023-06-19 16:14:07 -0400

    Fix Unit Tests

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-
-test('renders learn react link', () => {
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
