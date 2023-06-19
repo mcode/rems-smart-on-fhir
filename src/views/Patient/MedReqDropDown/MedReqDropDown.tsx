a4867c5ebcaf7b72319db7ad77fde2346a50d7c5
Zach Robin <zrobin144@gmail.com>
2023-06-19 13:14:50 -0700

    fixed merge conflict

--- src/views/Patient/MedReqDropDown/MedReqDropDown.tsx
+++ src/views/Patient/MedReqDropDown/MedReqDropDown.tsx
@@ -1,7 +1,5 @@
 import { Button, Card, CardContent, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Typography, Modal } from '@mui/material';
 import Box from '@mui/material/Box';
-
-
 import axios from 'axios';
 import { BundleEntry, Patient, MedicationRequest } from 'fhir/r4';
 import { useEffect, useState } from 'react';
@@ -38,7 +36,7 @@ function MedReqDropDown(props: any) {
 
     //For dropdown UI
     const [selectedOption, setSelectedOption] = useState<string>('');
-    
+
     const handleOptionSelect = (event: SelectChangeEvent<string>) => {
         setSelectedOption(event.target.value as string);
     };
@@ -63,12 +61,15 @@ function MedReqDropDown(props: any) {
     }, [client.patient, client]);
 
 
+    useEffect(() => {
+        getMedicationRequest();
+    }, []);
 
     //CDS-Hook Request to REMS-Admin for cards
     const buttonClickSubmitToREMS = () => {
         axios({
             method: 'post',
-            url: `${process.env.REACT_APP_REMS_ADMIN_SERVER_BASE}` +  `${process.env.REACT_APP_REMS_HOOKS_PATH}`,
+            url: `${process.env.REACT_APP_REMS_ADMIN_SERVER_BASE}` + `${process.env.REACT_APP_REMS_HOOKS_PATH}`,
             data: cdsHook
         })
             .then((response) => {
@@ -78,6 +79,10 @@ function MedReqDropDown(props: any) {
                 console.log(error);
             });
     };
+
+    useEffect(() => {
+        buttonClickSubmitToREMS();
+    }, [cdsHook]);
 
     const handleOpenCheckETASU = () => {
         setShowEtasu(true);
@@ -144,95 +149,94 @@ function MedReqDropDown(props: any) {
         border: '1px solid #000',
         boxShadow: 24,
         p: 4,
-      };
+    };
 
     const etasu_status_enabled: boolean = process.env.REACT_APP_ETASU_STATUS_ENABLED?.toLowerCase() === 'true' ? true : false;
     const pharmacy_status_enabled: boolean = process.env.REACT_APP_PHARMACY_STATUS_ENABLED?.toLowerCase() === 'true' ? true : false;
- 
+
     return (
         <div>
-        <Box sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        }}>
-            <div className='MedReqDropDown'>
-                <div>
-                    <Card sx={{ minWidth: 500, maxWidth: 5000, bgcolor: 'white', p: 5 }}>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 17 }} color='text.secondary' gutterBottom component='div'>
-                                Select Medication Request:
-                            </Typography>
-                            <FormControl sx={{ minWidth: 300, mt: 1 }}>
-                                <InputLabel id='dropdown-label'>Select Medication</InputLabel>
-                                <Select
-                                    labelId='dropdown-label'
-                                    id='dropdown'
-                                    value={selectedOption}
-                                    // TODO: --> onOpen might not be the best used here since it needs to be called twice to correctly return values
-                                    onOpen={getMedicationRequest}
-                                    onChange={handleOptionSelect}
-                                >
-                                    <MenuItem value=''>
-                                        <em>Select Medication</em>
-                                    </MenuItem>
-                                    {medication ?
-                                        medication.data.map((medications) => (
-                                            <MenuItem key={medications.id} value={medications.id}>
-                                                {medications.medicationCodeableConcept?.coding?.[0].display}
-                                            </MenuItem>
-
-                                        ))
-                                        : <p>loading medications...</p>}
-                                </Select>
-                            </FormControl>
-                        </CardContent>
-                        {selectedMedicationCard && (
+            <Box sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}>
+                <div className='MedReqDropDown'>
+                    <div>
+                        <Card sx={{ minWidth: 500, maxWidth: 5000, bgcolor: 'white', p: 5 }}>
                             <CardContent>
-                                <Typography sx={{ bgcolor: 'text.secondary', color: 'white', textAlign: 'center' }}>
-                                    Code: {selectedMedicationCard?.medicationCodeableConcept?.coding?.[0].code}
+                                <Typography sx={{ fontSize: 17 }} color='text.secondary' gutterBottom component='div'>
+                                    Select Medication Request:
                                 </Typography>
-                                <Typography sx={{ bgcolor: 'text.disabled', color: 'white', textAlign: 'center', fontSize: 24 }}>
-                                    {selectedMedicationCard?.medicationCodeableConcept?.coding?.[0].display?.split(' ')[0]}
-                                </Typography>
-                                <Typography variant='h6' sx={{ bgcolor: 'text.disabled', color: 'white', textAlign: 'center' }} color='textSecondary' gutterBottom>
-                                    {selectedMedicationCard?.medicationCodeableConcept?.coding?.[0].display}
-                                </Typography>
-                                <Button variant='contained' onClick={buttonClickSubmitToREMS}>Submit To REMS-Admin</Button>
-                                { etasu_status_enabled && (
-                                    <Button variant='contained' onClick={handleOpenCheckETASU}>Check ETASU</Button>
-                                )}
-                                { pharmacy_status_enabled && (
-                                    <Button variant='contained' onClick={handleOpenCheckPharmacy}>Check Pharmacy</Button>
-                                )}
+                                <FormControl sx={{ minWidth: 300, mt: 1 }}>
+                                    <InputLabel id='dropdown-label'>Select Medication</InputLabel>
+                                    <Select
+                                        labelId='dropdown-label'
+                                        id='dropdown'
+                                        value={selectedOption}
+                                        onChange={handleOptionSelect}
+                                    >
+                                        <MenuItem value=''>
+                                            <em>Select Medication</em>
+                                        </MenuItem>
+                                        {medication ?
+                                            medication.data.map((medications) => (
+                                                <MenuItem key={medications.id} value={medications.id}>
+                                                    {medications.medicationCodeableConcept?.coding?.[0].display}
+                                                </MenuItem>
+
+                                            ))
+                                            : <p>loading medications...</p>}
+                                    </Select>
+                                </FormControl>
                             </CardContent>
-                        )}
-                    </Card>
-                    <Card>
-                        <CardContent>
-                            <CdsHooksCards cards={hooksCards} client={client}></CdsHooksCards>
-                        </CardContent>
-                    </Card>
+                            {selectedMedicationCard && (
+                                <CardContent>
+                                    <Typography sx={{ bgcolor: 'text.secondary', color: 'white', textAlign: 'center' }}>
+                                        Code: {selectedMedicationCard?.medicationCodeableConcept?.coding?.[0].code}
+                                    </Typography>
+                                    <Typography sx={{ bgcolor: 'text.disabled', color: 'white', textAlign: 'center', fontSize: 24 }}>
+                                        {selectedMedicationCard?.medicationCodeableConcept?.coding?.[0].display?.split(' ')[0]}
+                                    </Typography>
+                                    <Typography variant='h6' sx={{ bgcolor: 'text.disabled', color: 'white', textAlign: 'center' }} color='textSecondary' gutterBottom>
+                                        {selectedMedicationCard?.medicationCodeableConcept?.coding?.[0].display}
+                                    </Typography>
+                                    {etasu_status_enabled && (
+                                        <Button variant='contained' onClick={handleOpenCheckETASU}>Check ETASU</Button>
+                                    )}
+                                    {pharmacy_status_enabled && (
+                                        <Button variant='contained' onClick={handleOpenCheckPharmacy}>Check Pharmacy</Button>
+                                    )}
+                                </CardContent>
+                            )}
+                        </Card>
+                        {selectedOption ?
+                            <Card>
+                                <CardContent>
+                                    <CdsHooksCards cards={hooksCards} client={client}></CdsHooksCards>
+                                </CardContent>
+                            </Card>
+                            : <p></p>}
+                    </div>
                 </div>
-            </div>
-        </Box >
-        <Modal
-            open={showEtasu}
-            onClose={handleCloseCheckETASU}
-        >
-            <Box sx={modal_style}>
-                <EtasuStatus patient={patient} medication={selectedMedicationCard} update={showEtasu}></EtasuStatus>
-            </Box>
-        </Modal>
-        <Modal
-            open={showPharmacy}
-            onClose={handleCloseCheckPharmacy}
-        >
-            <Box sx={modal_style}>
-                <PharmacyStatus patient={patient} medication={selectedMedicationCard} update={showPharmacy}></PharmacyStatus>
-            </Box>
-        </Modal>
+            </Box >
+            <Modal
+                open={showEtasu}
+                onClose={handleCloseCheckETASU}
+            >
+                <Box sx={modal_style}>
+                    <EtasuStatus patient={patient} medication={selectedMedicationCard} update={showEtasu}></EtasuStatus>
+                </Box>
+            </Modal>
+            <Modal
+                open={showPharmacy}
+                onClose={handleCloseCheckPharmacy}
+            >
+                <Box sx={modal_style}>
+                    <PharmacyStatus patient={patient} medication={selectedMedicationCard} update={showPharmacy}></PharmacyStatus>
+                </Box>
+            </Modal>
         </div>
     );
 }
