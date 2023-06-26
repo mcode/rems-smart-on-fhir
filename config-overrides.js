a60ebf2e05d1c9a7548b14ec2ed5ae9026830310
Robert A Dingwell <bobd@mitre.org>
2023-06-26 15:35:55 -0400

    Adding react-script-rewire to deal with polyfill issues

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+module.exports = function override (config, env) {
+  console.log('override')
+  let loaders = config.resolve
+  loaders.fallback = {
+      "fs": false,
+      "timers": require.resolve("timers-browserify") ,
+  }
+  
+  return config
+}
