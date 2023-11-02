3c8081e557597f1a802442969360b50159edbd07
Robert A Dingwell <bobd@mitre.org>
2023-11-02 12:59:34 -0400

    lint and prettier fixes

--- build.js
+++ build.js
@@ -5,8 +5,8 @@ const config = defaults.__get__('config');
 // Consolidate chunk files instead
 config.optimization.splitChunks = {
   cacheGroups: {
-    default: false,
-  },
+    default: false
+  }
 };
 // Move runtime into bundle instead of separate file
 config.optimization.runtimeChunk = false;
