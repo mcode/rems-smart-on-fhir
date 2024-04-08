024af0e8cec2e2ded10dd9762f3209b4729e5c71
KeeyanGhoreshi <keeyan.ghoreshi@uconn.edu>
2024-04-08 11:01:47 -0400

    prettier

--- src/views/Questionnaire/components/RemsInterface/RemsInterface.tsx
+++ src/views/Questionnaire/components/RemsInterface/RemsInterface.tsx
@@ -3,8 +3,7 @@ import ResourceEntry from './ResourceEntry';
 import './RemsInterface.css';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import {
-  Bundle} from 'fhir/r4';
+import { Bundle } from 'fhir/r4';
 
 interface RemsInterfaceProps {
   remsAdminResponse: RemsAdminResponse;
@@ -49,29 +48,29 @@ export default function RemsInterface(props: RemsInterfaceProps) {
   return (
     <div>
       <div>
-          <div>
-            <div className="left-form">
-              <h1>Document Status</h1>
-              <Paper style={{ paddingBottom: '5px' }}>
-                <div className="status-icon" style={{ backgroundColor: '#5cb85c' }}></div>
-                <div className="bundle-entry">Status: Documents successfully submitted</div>
-                <div className="bundle-entry">
-                  <Button variant="contained" onClick={toggleBundle}>
-                    View Bundle
-                  </Button>
-                </div>
-              </Paper>
-              {viewBundle ? (
-                <div className="bundle-view">
-                  <br></br>
-                  <h3>Bundle</h3>
-                  {renderBundle(props.specialtyRxBundle)}
-                </div>
-              ) : (
-                ''
-              )}
-            </div>
+        <div>
+          <div className="left-form">
+            <h1>Document Status</h1>
+            <Paper style={{ paddingBottom: '5px' }}>
+              <div className="status-icon" style={{ backgroundColor: '#5cb85c' }}></div>
+              <div className="bundle-entry">Status: Documents successfully submitted</div>
+              <div className="bundle-entry">
+                <Button variant="contained" onClick={toggleBundle}>
+                  View Bundle
+                </Button>
+              </div>
+            </Paper>
+            {viewBundle ? (
+              <div className="bundle-view">
+                <br></br>
+                <h3>Bundle</h3>
+                {renderBundle(props.specialtyRxBundle)}
+              </div>
+            ) : (
+              ''
+            )}
           </div>
+        </div>
       </div>
     </div>
   );
