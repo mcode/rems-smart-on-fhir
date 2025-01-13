508a8dfb299f30a8078636eb6cb9d3f563712b32
Sahil Malhotra <smalhotra@mitre.org>
2025-01-13 13:51:58 -0500

    redeploy for connectathon

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // load env vars from .env
 export default defineConfig({
-  // depending on your application, base can also be "/"
-  base: '',
   plugins: [react(), viteTsconfigPaths(), nodePolyfills()],
   define: {
     'process.env': process.env
   },
+  // depending on your application, base can also be "/"
+  base: process.env.REACT_APP_VITE_BASE || '',
   server: {
     port: parseInt(process.env.PORT!),
     open: false,
